f81e925d0faf328a86d2aeddd197a6353f94b337
Jose Villegas <dev@duikerbok.com>
2024-12-23 19:24:25 -0800

    Stack colors in compact size.

--- GenerativeArt/Application/ColorPickerView.swift
+++ GenerativeArt/Application/ColorPickerView.swift
@@ -4,57 +4,61 @@ struct ColorPickerView: View {
   @Binding var selectedColor: Color
   let horizontalSizeClass: UserInterfaceSizeClass?
 
-  private let colors: [Color] = [.black, .white, .red, .orange, .green, .yellow, .blue, .purple]
+  private let colors1: [Color] = [.black, .white, .red, .orange]
+  private let colors2: [Color] = [.green, .yellow, .blue, .purple]
 
   var body: some View {
-    HStack(spacing: horizontalSizeClass == .compact ? 0 : 8) {
+    Group {
+      if horizontalSizeClass == .compact {
+        VStack(spacing: 8) {
+          ColorPickerRow(colors: colors1, selectedColor: $selectedColor)
+          ColorPickerRow(colors: colors2, selectedColor: $selectedColor)
+        }
+      } else {
+        ColorPickerRow(colors: colors1 + colors2, selectedColor: $selectedColor)
+      }
+    }
+    .padding(16)
+  }
+}
+
+struct ColorPickerRow: View {
+  let colors: [Color]
+  @Binding var selectedColor: Color
+
+  var body: some View {
+    HStack(spacing: 8) {
       ForEach(colors, id: \.hashValue) { color in
-        ColorPickerItemView(color: color, isSelected: color == selectedColor, horizontalSizeClass: horizontalSizeClass)
+        ColorPickerItemView(color: color, isSelected: color == selectedColor)
           .onTapGesture { selectedColor = color }
       }
     }
-    .padding(.horizontal, 16)
   }
 }
 
 struct ColorPickerItemView: View {
   let color: Color
   let isSelected: Bool
-  let horizontalSizeClass: UserInterfaceSizeClass?
+
+  private let itemSize: CGFloat = 44
 
   var body: some View {
     ZStack {
       Circle()
         .fill(color)
         .stroke(strokeColor, style: StrokeStyle(lineWidth: 1))
-        .frame(width: fillCircleSize, height: fillCircleSize)
+        .frame(width: itemSize + selectionPadding, height: itemSize + selectionPadding)
       Circle()
         .fill(.clear)
-        .strokeBorder(lineWidth: selectionBorderWidth)
+        .strokeBorder(lineWidth: 4)
         .foregroundStyle(selectionColor)
-        .frame(width: strokeCircleSize, height: strokeCircleSize)
+        .frame(width: itemSize + 8, height: itemSize + 8)
     }
-  }
-
-  private var fillCircleSize: CGFloat {
-    itemSize + selectionPadding
-  }
-
-  private var strokeCircleSize: CGFloat {
-    itemSize + selectionBorderWidth * 2
-  }
-
-  private var itemSize: CGFloat {
-    horizontalSizeClass == .compact ? 34 : 44
   }
 
   // Prevents artifacts from appearing between color circle and selection border.
   private var selectionPadding: CGFloat {
     isSelected ? 4 : 0
-  }
-
-  private var selectionBorderWidth: CGFloat {
-    horizontalSizeClass == .compact ? 3 : 4
   }
 
   private var selectionColor: Color {
