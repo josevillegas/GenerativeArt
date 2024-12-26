91d54daf913f7e50c3030424d6af30d0df2b1024
Jose Villegas <dev@duikerbok.com>
2024-12-25 18:22:30 -0800

    Move views to one file.

--- GenerativeArt/Application/DrawingView.swift
+++ GenerativeArt/Application/DrawingView.swift
@@ -1,10 +1,47 @@
 import SwiftUI
 
-struct DrawingViewSizePreferenceKey: PreferenceKey {
-  static var defaultValue: CGSize = .zero
+struct DrawingNavigationView: View {
+  @Binding var selectedDrawingType: DrawingType?
+  @Binding var splitViewVisibility: NavigationSplitViewVisibility
+  let tiledDrawingType: TiledDrawingTypeWrapper
+  let mondrianDrawing: MondrianDrawing
+  let foregroundColor: Color
+  let backgroundColor: Color
+  let tileSize: CGFloat
+  let isPlaying: Bool
+  let perform: (ToolbarAction) -> Void
 
-  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
-    value = nextValue()
+  static let defaultDrawingType: DrawingType = .tile(.triangles)
+
+  @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+
+  var body: some View {
+    if horizontalSizeClass == .compact {
+      NavigationStack {
+        SidebarView(selectedDrawingType: $selectedDrawingType)
+          .navigationDestination(item: $selectedDrawingType) { drawingType in
+            DrawingView(drawingType: drawingType, tiledDrawingType: tiledDrawingType, mondrianDrawing: mondrianDrawing,
+                        foregroundColor: foregroundColor, backgroundColor: backgroundColor, tileSize: tileSize)
+            .modifier(ToolbarModifier(type: drawingType, foregroundColor: foregroundColor, backgroundColor: backgroundColor,
+                                      tileSize: tileSize, isPlaying: isPlaying, perform: perform))
+            .modifier(NavigationBarModifier())
+          }
+      }
+    } else {
+      NavigationSplitView(columnVisibility: $splitViewVisibility) {
+        SidebarView(selectedDrawingType: $selectedDrawingType)
+          .navigationTitle("Generative Art")
+          .toolbar(removing: .sidebarToggle)
+      } detail: {
+        DrawingView(drawingType: selectedDrawingType ?? Self.defaultDrawingType, tiledDrawingType: tiledDrawingType,
+                    mondrianDrawing: mondrianDrawing, foregroundColor: foregroundColor, backgroundColor: backgroundColor,
+                    tileSize: tileSize)
+        .modifier(ToolbarModifier(type: selectedDrawingType ?? Self.defaultDrawingType, foregroundColor: foregroundColor,
+                                  backgroundColor: backgroundColor, tileSize: tileSize, isPlaying: isPlaying, perform: perform))
+        .modifier(NavigationBarModifier())
+      }
+      .navigationSplitViewStyle(.prominentDetail)
+    }
   }
 }
 
@@ -27,5 +64,13 @@ struct DrawingView: View {
       }
       .preference(key: DrawingViewSizePreferenceKey.self, value: proxy.size)
     }
+  }
+}
+
+struct DrawingViewSizePreferenceKey: PreferenceKey {
+  static var defaultValue: CGSize = .zero
+
+  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+    value = nextValue()
   }
 }
