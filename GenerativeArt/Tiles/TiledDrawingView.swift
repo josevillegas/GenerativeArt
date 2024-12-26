e39739ffaac7d6a7c4abe134a13723a6e316a764
Jose Villegas <dev@duikerbok.com>
2024-12-25 22:16:43 -0800

    Move TiledDrawing up to TiledDrawingView.

--- GenerativeArt/Tiles/TiledDrawingView.swift
+++ GenerativeArt/Tiles/TiledDrawingView.swift
@@ -7,30 +7,35 @@ struct TiledDrawingView: UIViewRepresentable {
   let backgroundColor: Color
 
   func makeUIView(context: Context) -> TiledDrawingUIView {
-    TiledDrawingUIView(tiledDrawing: TiledDrawing(type: type.type, tiles: tiles))
+    let view = TiledDrawingUIView()
+    view.canvas.tiledDrawing = TiledDrawing(type: type.type, tiles: tiles)
+    return view
   }
 
   func updateUIView(_ view: TiledDrawingUIView, context: Context) {
-    view.canvas.tiledDrawing.tiles = tiles
-    view.canvas.tiledDrawing.foregroundColor = foregroundColor
-    view.canvas.tiledDrawing.backgroundColor = backgroundColor
-    view.canvas.tiledDrawing.type = type.type
-    view.canvas.tiledDrawing.updateVariations()
-    view.canvas.setNeedsDisplay()
+    guard var tiledDrawing = view.canvas.tiledDrawing else { return }
 
-    let panelSize = view.canvas.tiledDrawing.tiles.size
+    tiledDrawing.tiles = tiles
+    tiledDrawing.foregroundColor = foregroundColor
+    tiledDrawing.backgroundColor = backgroundColor
+    tiledDrawing.type = type.type
+    tiledDrawing.updateVariations()
+    view.canvas.tiledDrawing = tiledDrawing
+
+    let panelSize = tiledDrawing.tiles.size
     view.panelWidthConstraint.constant = panelSize.width
     view.panelHeightConstraint.constant = panelSize.height
+
+    view.canvas.setNeedsDisplay()
   }
 }
 
 final class TiledDrawingUIView: UIView {
-  let canvas: TiledDrawingCanvas
+  let canvas = TiledDrawingCanvas()
   let panelWidthConstraint: NSLayoutConstraint
   let panelHeightConstraint: NSLayoutConstraint
 
-  init(tiledDrawing: TiledDrawing) {
-    canvas = TiledDrawingCanvas(tiledDrawing: tiledDrawing)
+  init() {
     panelWidthConstraint = canvas.widthAnchor.constraint(equalToConstant: 0)
     panelHeightConstraint = canvas.heightAnchor.constraint(equalToConstant: 0)
     super.init(frame: .zero)
@@ -54,18 +59,9 @@ final class TiledDrawingUIView: UIView {
 }
 
 final class TiledDrawingCanvas: UIView {
-  var tiledDrawing: TiledDrawing
-
-  init(tiledDrawing: TiledDrawing) {
-    self.tiledDrawing = tiledDrawing
-    super.init(frame: .zero)
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+  var tiledDrawing: TiledDrawing?
 
   override func draw(_: CGRect) {
-    tiledDrawing.paths.draw()
+    tiledDrawing?.paths.draw()
   }
 }
